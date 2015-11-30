a9a5db1cd7b6c2df5831823cde62a9f576c6e70b
Junnama Noda <junnama@alfasado.jp>
2015-11-30 11:52:36 +0900

    developer_tagfilter => objectloop_tag_filter.

--- plugins/ObjectLoop/php/tagfilter/objectloop_tag_filter_mtentrymodel.php
+++ plugins/ObjectLoop/php/tagfilter/objectloop_tag_filter_mtentrymodel.php
@@ -1,5 +1,5 @@
 <?php
-function developer_tag_filter_mtentrymodel ( $mt, &$ctx, &$args, $content = NULL ) {
+function objectloop_tag_filter_mtentrymodel ( $mt, &$ctx, &$args, $content = NULL ) {
     /*
     if ( $ctx->stash( 'filter_where' ) ) {
         $ctx->stash( 'filter_extras', array( 'limit' => 1 ) );
